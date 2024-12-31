2a9e36cfadbc46c54144b90f6ab33b74e339b2fc
RaviKumarGU <ravikumarfs016@gmail.com>
2024-12-31 16:14:32 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import ReactAutocomplete from "react-autocomplete";
+
 function App() {
-  return <div className="App">Hello World</div>;
+  const [value, setValue] = useState();
+  return (
+    <ReactAutocomplete
+      items={[
+        { id: "foo", label: "foo" },
+        { id: "bar", label: "bar" },
+        { id: "baz", label: "baz" },
+      ]}
+      shouldItemRender={(item, value) =>
+        item.label.toLowerCase().indexOf(value.toLowerCase()) > -1
+      }
+      getItemValue={(item) => item.label}
+      renderItem={(item, highlighted) => (
+        <div
+          key={item.id}
+          style={{ backgroundColor: highlighted ? "#eee" : "transparent" }}
+        >
+          {item.label}
+        </div>
+      )}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      onSelect={(value) => setValue({ value })}
+    />
+  );
 }
 
 export default App;
